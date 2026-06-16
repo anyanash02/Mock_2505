05bae7fd173e1ee143e430905ccdf6c2cc245df2
anyanash02 <anyanash02@gmail.com>
2026-06-16 12:56:12 +0200

    Initial mock exam project

---
+++ code/push_to_github.R
@@ -0,0 +1,19 @@
+# ============================================================
+# Connect local RStudio project to GitHub - RUN THIS IN TERMINAL
+# ============================================================
+git init
+git status
+git add .
+git commit -m "Initial mock exam project"
+git branch -M main
+git remote add origin https://github.com/anyanash02/Mock_2505.git
+git remote -v
+git push -u origin main
+
+
+
+# push throughout exam
+git status
+git add .
+git commit -m "Update question 1"
+git push
